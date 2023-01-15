d0ce66b0d456038f5d5195dddf611717a096669f
Nat Pryce <sw@natpryce.com>
2023-01-15 22:11:49 +0000

    Test: first player bowls a strike (failing but compiling)

--- src/test/kotlin/com/natpryce/kotlinconf2023/bowling/NewGameTest.kt
+++ src/test/kotlin/com/natpryce/kotlinconf2023/bowling/NewGameTest.kt
@@ -10,6 +10,16 @@ class NewGameTest {
         assertEquals(0, game.nextPlayerToBowl())
     }
     
+    @Test
+    fun `first player bowls a strike`() {
+        val game = newGame().afterRoll(10)
+        
+        assertEquals(1, game.nextPlayerToBowl())
+    }
+}
+
+private fun BowlingGame.afterRoll(score: Int): BowlingGame {
+    TODO("Not yet implemented")
 }
 
 private fun BowlingGame.nextPlayerToBowl(): Int = 0
