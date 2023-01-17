9630685b4ddc44412a08a3b669f9e56d71ac931f
Nat Pryce <sw@natpryce.com>
2023-01-17 23:45:07 +0000

    Impl: had to model the game as a list of Frames per player to keep track of scores

--- src/main/kotlin/com/natpryce/kotlinconf2023/bowling/bowling.kt
+++ src/main/kotlin/com/natpryce/kotlinconf2023/bowling/bowling.kt
@@ -1,19 +1,66 @@
 package com.natpryce.kotlinconf2023.bowling
 
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.plus
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
+
+data class Frame(
+    val scores: PersistentList<Int>
+)
+
+val unplayedFrame = Frame(persistentListOf())
+
+private fun Frame.isIncomplete(): Boolean =
+    !isComplete()
+
+private fun Frame.isComplete(): Boolean =
+    scores.size == 2 || pins() == 10
+
+private fun Frame.pins() =
+    scores.sum()
+
+private fun Frame.plusScore(score: Int): Frame =
+    copy(scores = scores + score)
+
+
+
+// Frames played by a single player in the game
+typealias PlayerFrames = PersistentList<Frame>
+
+val newPlayerFrames = persistentListOf(unplayedFrame)
+
+private fun PlayerFrames.plusScore(score: Int) =
+    set(0, this[0].plusScore(score))
+
+// A game played by two or more players
 data class BowlingGame(
     val playerCount: Int,
-    val rollsByPlayer : List<Int> = emptyList()
+    val playerFrames: PersistentList<PlayerFrames> =
+        (1..playerCount).map { newPlayerFrames }.toPersistentList()
 )
 
 fun newGame(playerCount: Int) =
     BowlingGame(playerCount = playerCount)
 
-fun BowlingGame.afterRoll(score: Int): BowlingGame =
-    copy(rollsByPlayer = rollsByPlayer + score)
+fun PersistentList<PlayerFrames>.withNewFrames() =
+    map { it + unplayedFrame }.toPersistentList()
+
+fun BowlingGame.afterRoll(score: Int): BowlingGame {
+    val player = nextPlayerToBowl()
+    val frames = if (player == 0) playerFrames.withNewFrames() else playerFrames
+    
+    return copy(
+        playerFrames = frames.plusScoreForPlayer(player, score)
+    )
+}
+
+private fun PersistentList<PlayerFrames>.plusScoreForPlayer(player: Int, score: Int) =
+    set(player, get(player).plusScore(score))
 
 fun BowlingGame.nextPlayerToBowl(): Int =
-    if (rollsByPlayer.isNotEmpty() && rollsByPlayer.last() < 10) {
-        rollsByPlayer.lastIndex
-    } else {
-        rollsByPlayer.size % playerCount
-    }
+    playerFrames
+        .indexOfFirst { it[0].isIncomplete() }
+        .takeUnless { it < 0 }
+        ?: 0
+
