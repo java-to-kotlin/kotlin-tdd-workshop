158e550ba987d4317826285a9ab157dc5ca4b07b
Nat Pryce <sw@natpryce.com>
2023-01-19 07:19:19 +0000

    Refactor Tests: clarify and remove some "cleverness"

--- src/test/kotlin/com/natpryce/kotlinconf2023/bowling/PlayerTurnsTest.kt
+++ src/test/kotlin/com/natpryce/kotlinconf2023/bowling/PlayerTurnsTest.kt
@@ -2,12 +2,8 @@ package com.natpryce.kotlinconf2023.bowling
 
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.property.Arb
-import io.kotest.property.arbitrary.flatMap
-import io.kotest.property.arbitrary.int
-import io.kotest.property.arbitrary.map
 import io.kotest.property.arbitrary.next
 import io.kotest.property.checkAll
-
 
 
 fun <T> T.repeated(times: Int, f: (T) -> T): T {
@@ -30,38 +26,33 @@ class PlayerTurnsTest : AnnotationSpec() {
     @Test
     suspend fun `all players bowl a strike, back to first player`() {
         checkAll(Arb.bowlingGame()) { newGame ->
-            newGame
-                .repeated(times = newGame.playerCount - 1) { beforeRoll ->
+            val finalRoll =
+                newGame.repeated(times = newGame.playerCount - 1) { beforeRoll ->
                     val player = beforeRoll.nextPlayerToBowl()
                     
-                    val result = beforeRoll.afterRoll(10)
+                    val afterRoll = beforeRoll.afterRoll(10)
                     
-                    assert(result.nextPlayerToBowl() == player + 1)
+                    assert(afterRoll.nextPlayerToBowl() == player + 1)
                     
-                    result
-                    
+                    afterRoll
                 }
-                .then { finalRoll ->
-                    assert(finalRoll.afterRoll(10).nextPlayerToBowl() == 0)
-                }
+            
+            assert(finalRoll.afterRoll(10).nextPlayerToBowl() == 0)
         }
     }
     
     @Test
     suspend fun `players score less than 10 in first roll, back to first player`() {
         checkAll(Arb.bowlingGame()) { newGame ->
-            newGame
+            val finalRoll = newGame
                 .repeated(times = newGame.playerCount) {
                     val (first, second) = Arb.frame(maxPinsInFirstRoll = 9).next()
                     
                     it.afterRoll(first).afterRoll(second)
                 }
-                .then {
-                    assert(it.nextPlayerToBowl() == 0)
-                }
+            
+            assert(finalRoll.nextPlayerToBowl() == 0)
         }
     }
 }
-
-inline fun <T> T.then(statement: (T) -> Unit): T = also(statement)
 
