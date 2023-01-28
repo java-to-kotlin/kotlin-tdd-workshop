364aca8f4c5d2ecab9905b56b616bed1366ac7f4
Nat Pryce <sw@natpryce.com>
2023-01-28 12:34:58 +0000

    Test: new game, no frames, zero total score

---
+++ src/test/kotlin/example/bowling/ScoringTest.kt
@@ -0,0 +1,19 @@
+package example.bowling
+
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class ScoringTest {
+    @Test
+    fun `new game - no frames, total score 0`() {
+        val game = newGame
+        assertTrue(game.totalScore() == 0)
+        assertTrue(game.isEmpty())
+    }
+}
+
+val newGame = persistentListOf<Any>()
+
+private fun PersistentList<Any>.totalScore(): Int = TODO()
