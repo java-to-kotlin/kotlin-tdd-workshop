15c69a6a4dc74e7914148db02feffe708128e506
Nat Pryce <sw@natpryce.com>
2023-01-28 12:26:17 +0000

    Some optional libraries that participants may find useful

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest:kotest-property:5.5.4")
 }
 
 tasks.test {
