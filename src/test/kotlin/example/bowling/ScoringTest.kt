a925a47dcfadb02a1880ba8c6729d5326c88538f
Nat Pryce <sw@natpryce.com>
2023-01-29 02:01:54 +0000

    Test: two rolls, below 10

--- src/test/kotlin/example/bowling/ScoringTest.kt
+++ src/test/kotlin/example/bowling/ScoringTest.kt
@@ -21,19 +21,35 @@ class ScoringTest : AnnotationSpec() {
         checkAll(Arb.int(0..9)) { n ->
             val game = newGame.roll(n)
             assertTrue(game.totalScore() == n)
-            assertTrue(game.frames() == persistentListOf(Frame(pinsDown = n)))
+            assertTrue(game.frames() == persistentListOf(IncompleteFrame(pinsDown = n)))
         }
+    }
+    
+    @Test
+    fun `two rolls not a spare`() {
+        val game = newGame.roll(4).roll(3)
+        
+        assertTrue(game.totalScore() == 7)
+        assertTrue(game.frames() == persistentListOf(CompleteFrame(firstRoll = 4, secondRoll = 3)))
     }
 }
 
-data class Frame(val pinsDown: Int)
+sealed interface Frame {
+    val pinsDown: Int
+}
+
+data class IncompleteFrame(override val pinsDown: Int) : Frame
+
+data class CompleteFrame(val firstRoll: Int, val secondRoll: Int) : Frame {
+    override val pinsDown: Int get() = TODO()
+}
 
 typealias Game = PersistentList<Frame>
 
 val newGame = persistentListOf<Frame>()
 
 private fun Game.roll(pinsDown: Int): Game =
-    this + Frame(pinsDown)
+    this + IncompleteFrame(pinsDown)
 
 private fun Game.frames() = this
 
