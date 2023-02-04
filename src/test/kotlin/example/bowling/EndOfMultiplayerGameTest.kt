0d61e895bb3fc6f44184aff14bf0debc7792fe37
Nat Pryce <sw@natpryce.com>
2023-02-04 15:17:37 +0100

    Test: end of multiplayer game after all open frames, shows gap in dummy implementation

--- src/test/kotlin/example/bowling/EndOfMultiplayerGameTest.kt
+++ src/test/kotlin/example/bowling/EndOfMultiplayerGameTest.kt
@@ -9,7 +9,11 @@ class EndOfMultiplayerGameTest : AnnotationSpec() {
     fun `game is over when all players' last frame is an open frame`() {
         val game = (1..10).fold(newGame(3)) { game, _ ->
             (1..3).fold(game) { turn, _ ->
-                turn.roll(1).roll(2)
+                turn
+                    .also { assertTrue(!it.isOver()) }
+                    .roll(1)
+                    .also { assertTrue(!it.isOver()) }
+                    .roll(2)
             }
         }
         
