6b0aef1f5ec762849be9148d5f876a9430ed3851
Duncan McGregor <duncan@oneeyedmen.com>
2023-03-07 22:08:25 +0000

    Frames and lines and (non-cumulative) scores

--- src/main/kotlin/dev/javaToKotlin/types.kt
+++ src/main/kotlin/dev/javaToKotlin/types.kt
@@ -1,7 +1,7 @@
 package dev.javaToKotlin
 
 @JvmInline
-value class PinCount(val value: Int) {
+value class PinCount private constructor(val value: Int) {
     init {
         require(value in 0..10)
     }
@@ -13,17 +13,130 @@ value class PinCount(val value: Int) {
                 else -> null
             }
     }
+
+    override fun toString() = value.toString()
+
+    fun scorecard() = when (value) {
+        10 -> "X"
+        else -> value.toString()
+    }
 }
 
 class Score
-
-class Frame
 
 class Game(val lines: List<Line>)
 
 typealias Player = String
 
-class Line(
+sealed class Frame {
+    abstract fun scorecard(): String
+}
+
+interface PlayableFrame {
+    fun roll(pins: PinCount): Frame
+}
+
+class UnplayedFrame : Frame(), PlayableFrame {
+    override fun roll(pins: PinCount): Frame =
+        when (pins.value) {
+            10 -> Strike()
+            else -> InProgressFrame(pins)
+        }
+
+    override fun scorecard() = " , "
+}
+
+class UnplayedFinalFrame : Frame(), PlayableFrame {
+    override fun roll(pins: PinCount) = InProgressFinalFrame(pins)
+    override fun scorecard() = " , , "
+}
+
+class InProgressFinalFrame(val pins: PinCount) : Frame(), PlayableFrame {
+    override fun roll(pins: PinCount): Frame =
+        when {
+            this.pins.value + pins.value >= 10 -> FinalBonusFrame(this.pins, pins)
+            else -> CompletedFinalFrame(this.pins, pins)
+        }
+    override fun scorecard() = "${pins.scorecard()}, , "
+}
+
+
+class FinalBonusFrame(
+    val roll1: PinCount,
+    val roll2: PinCount
+) : Frame(), PlayableFrame {
+    override fun roll(pins: PinCount) = CompletedFinalFrame(roll1, roll2, pins)
+    override fun scorecard() = maybeSpare(roll1, roll2) + ", "
+}
+
+fun maybeSpare(roll1: PinCount, roll2: PinCount) = "${roll1.scorecard()}," +
+        (if (roll1.value != 10 && roll1.value + roll2.value == 10) "/" else roll2.scorecard())
+
+class InProgressFrame(val pins: PinCount) : Frame(), PlayableFrame {
+    override fun roll(pins: PinCount) = CompletedFrame(this.pins, pins)
+    override fun scorecard() = "${pins.scorecard()}, "
+}
+
+class CompletedFinalFrame(
+    val roll1: PinCount,
+    val roll2: PinCount,
+    val roll3: PinCount? = null
+) : Frame() {
+    override fun scorecard() = maybeSpare(roll1, roll2) + "," +
+            (roll3?.scorecard() ?: " ")
+}
+
+class CompletedFrame(
+    val roll1: PinCount,
+    val roll2: PinCount
+) : Frame() {
+    override fun scorecard() = maybeSpare(roll1, roll2)
+}
+
+class Strike : Frame() {
+    override fun scorecard() = " ,X"
+}
+
+fun lineFor(
+    player: Player,
+    frameCount: Int
+): Line = when {
+    frameCount > 0 -> PlayableLine(
+        player,
+        List(frameCount - 1) { UnplayedFrame() } + UnplayedFinalFrame()
+    )
+
+    else -> CompletedLine(player, emptyList())
+}
+
+sealed class Line(
     val player: Player,
     val frames: List<Frame>
-)
+) {
+    fun scorecard(): String =
+        (listOf("$player") + frames.map { it.scorecard() }).joinToString("|")
+}
+
+class PlayableLine(
+    player: Player,
+    frames: List<Frame>
+) : Line(player, frames) {
+    fun roll(pinCount: PinCount): Line {
+        val currentFrame = frames.first { it is PlayableFrame }
+        if (currentFrame !is PlayableFrame)
+            error("Unexpectedly unplayable frame")
+        val newFrame = currentFrame.roll(pinCount)
+        val newFrames = frames.toMutableList().apply {
+            this[this.indexOf(currentFrame)] = newFrame
+        }.toList()
+        return if (newFrames.last() is PlayableFrame)
+            PlayableLine(player, newFrames)
+        else
+            CompletedLine(player, newFrames)
+    }
+}
+
+class CompletedLine(
+    player: Player,
+    frames: List<Frame>
+) : Line(player, frames)
---
+++ src/test/kotlin/dev/javaToKotlin/Tests.kt
@@ -0,0 +1,153 @@
+package dev.javaToKotlin
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class Tests {
+    @Test
+    fun `zero frame game`() {
+        val line = lineFor("Fred", frameCount = 0)
+        assertEquals("Fred", line.scorecard())
+        assertFalse(line is PlayableLine)
+    }
+
+    @Test
+    fun `single frame game`() {
+        var line = lineFor("Fred", frameCount = 1)
+        assertEquals("Fred| , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(4.pins)
+        assertEquals("Fred|4, , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(2.pins)
+        assertEquals("Fred|4,2, ", line.scorecard())
+        assertTrue(line is CompletedLine)
+    }
+
+    @Test
+    fun `two frame game`() {
+        var line = lineFor("Fred", frameCount = 2)
+        assertEquals("Fred| , | , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(4.pins)
+        assertEquals("Fred|4, | , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(2.pins)
+        assertEquals("Fred|4,2| , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(5.pins)
+        assertEquals("Fred|4,2|5, , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(3.pins)
+        assertEquals("Fred|4,2|5,3, ", line.scorecard())
+        assertTrue(line is CompletedLine)
+    }
+
+    @Test
+    fun `two frame game with initial strike`() {
+        var line = lineFor("Fred", frameCount = 2)
+        assertEquals("Fred| , | , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(10.pins)
+        assertEquals("Fred| ,X| , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(5.pins)
+        assertEquals("Fred| ,X|5, , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(3.pins)
+        assertEquals("Fred| ,X|5,3, ", line.scorecard())
+        assertTrue(line is CompletedLine)
+    }
+
+    @Test
+    fun `two frame game with initial spare`() {
+        var line = lineFor("Fred", frameCount = 2)
+        assertEquals("Fred| , | , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(9.pins)
+        assertEquals("Fred|9, | , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(1.pins)
+        assertEquals("Fred|9,/| , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(5.pins)
+        assertEquals("Fred|9,/|5, , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(3.pins)
+        assertEquals("Fred|9,/|5,3, ", line.scorecard())
+        assertTrue(line is CompletedLine)
+    }
+
+    @Test
+    fun `strike in final frame gives another two rolls`() {
+        var line = lineFor("Fred", frameCount = 1)
+        assertEquals("Fred| , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(10.pins)
+        assertEquals("Fred|X, , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(5.pins)
+        assertEquals("Fred|X,5, ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(3.pins)
+        assertEquals("Fred|X,5,3", line.scorecard())
+        assertTrue(line is CompletedLine)
+    }
+
+    @Test
+    fun `spare in final frame gives another roll`() {
+        var line = lineFor("Fred", frameCount = 1)
+        assertEquals("Fred| , , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(9.pins)
+        assertEquals("Fred|9, , ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(1.pins)
+        assertEquals("Fred|9,/, ", line.scorecard())
+        assertTrue(line is PlayableLine)
+
+        line = line.roll(3.pins)
+        assertEquals("Fred|9,/,3", line.scorecard())
+        assertTrue(line is CompletedLine)
+    }
+
+    @Test fun `two rolls on UnplayedFrame`() {
+        val frame = UnplayedFrame()
+
+        val frame2 = frame.roll(4.pins)
+        assertTrue(frame2 is InProgressFrame)
+
+        val frame3 = frame2.roll(2.pins)
+        assertTrue(frame3 is CompletedFrame)
+    }
+
+    @Test fun `strike on UnplayedFrame`() {
+        val frame = UnplayedFrame()
+
+        val frame2 = frame.roll(10.pins)
+        assertTrue(frame2 is Strike)
+    }
+}
+
+val Int.pins: PinCount
+    get() = PinCount(this) ?: error("Invalid pincount value of ${this}")
