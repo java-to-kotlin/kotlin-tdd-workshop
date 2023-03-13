d31e80bb2591346ca83a413a71c3b91158aaea91
Nat Pryce <sw@natpryce.com>
2023-03-13 11:23:13 +0000

    Sketch types

---
+++ src/main/kotlin/dev/javaToKotlin/types.kt
@@ -0,0 +1,24 @@
+package dev.javaToKotlin
+
+
+class Game
+class Frame
+class PinCount
+class Score
+class Player
+
+
+
+
+class Strike
+class Spare
+class Turn
+class MatchState
+class Result
+class Roll
+class PerfectGame
+class Winner
+class PlayerQueue
+class Draw
+class Bonus
+class CurrentFrame
